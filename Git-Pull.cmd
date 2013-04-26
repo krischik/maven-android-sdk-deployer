29f2fd3a78468da2b8ce4e9b0d9ae94bd75725e4
Martin Krischik <martin.krischik@noser.com>
2013-04-26 13:22:53 +0200

    update pull script to scala

---
+++ Git-Pull.cmd
@@ -0,0 +1,30 @@
+::#!
+::::::::::::::::::::::::::::::::::::::::::::::::::::::::::: {{{1 :::::::^::::
+:: Copyright © 2013 Martin Krischik
+::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::
+:: $Author$
+:: $Revision$
+:: $Date$
+:: $Id$
+:: $HeadURL$
+::::::::::::::::::::::::::::::::::::::::::::::::::::::::::: }}}1 :::::::::::
+@ECHO OFF
+
+SETLOCAL
+    SET PATH=%PATH%;C:\opt\Git\bin
+    SET PATH=%PATH%;C:\opt\Scala\2.10.0\bin
+
+    CALL scala -save %~f0 %*    
+ENDLOCAL
+    
+GOTO :eof
+::!#
+
+import scala.sys.process._
+
+"git" :: "pull" :: "https://github.com/mosabua/maven-android-sdk-deployer.git" :: Nil !;
+"git" :: "pull" :: "https://github.com/krischik/maven-android-sdk-deployer.git" :: Nil !;
+"git" :: "push" :: Nil !;
+
+// vim: set wrap tabstop=8 shiftwidth=4 softtabstop=4 noexpandtab :
+// vim: set textwidth=0 filetype=scala foldmethod=marker nospell :
